9048d2af04a90f97e0c2af6d48ace7c65f41527c
tracy <tracyjacobs775@gmail.com>
2022-03-14 12:47:10 +0300

    add h1

--- app/main/views.py
+++ app/main/views.py
@@ -7,7 +7,7 @@ from ..request import get_source,article_source,get_category,get_headlines
 @main.route('/')
 def index():
     '''
-    Root function returning index/home page with data
+    Root function returning home page with data
     '''
     source= get_source()
     headlines = get_headlines()
--- config.py
+++ config.py
@@ -5,7 +5,6 @@ class Config:
     General configuration parent class
     '''
     NEWS_API_SOURCE_URL='https://newsapi.org/v2/sources?apiKey={}'
-    # CAT_API_URL='https://newsapi.org/v2/everything?q={}&sortBy=relevancy&apiKey={}'
     NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
     CAT_API_URL='https://newsapi.org/v2/top-headlines?country=us&category={}&apiKey={}'
 
